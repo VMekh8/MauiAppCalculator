8c329b5b47bdf9b296e7cc55f89d01daacbc8dea
VMekh8 <mekhvitaliy88@gmail.com>
2024-02-12 23:39:43 +0200

    Add project files.

---
+++ MauiAppCalculator/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppCalculator
+{
+    public static class Calculate
+    {
+        public static double Calc(double n1, double n2, string MathOperator)
+        {
+
+            switch (MathOperator)
+            {
+                case "*": 
+                    return n1 * n2;
+
+                case "/":
+                    return n1 / n2; 
+
+                case "+":
+                    return n1 + n2; 
+
+                case "-":
+                    return n1 - n2; 
+
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ MauiAppCalculator/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿
+using System.Diagnostics;
+
+namespace MauiAppCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int CurrentState = 1;
+        double number1, number2;
+        string MathOperator;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            OnClear(this, null);
+        }
+        
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            number1 = -1;
+            number2 = -1;
+            MathOperator = string.Empty;
+            CurrentState = 1;
+        }
+
+        private void OnNumberInput(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string btnpress = button.Text;
+
+            if (result.Text == "0" || CurrentState <0)
+            {
+                result.Text = string.Empty;
+ 
+            }
+            if(CurrentState % 2 == 0) 
+                CurrentState *= -1;
+            result.Text += btnpress;
+
+            if (CurrentState == 1)
+            {
+                number1 = Convert.ToDouble(result.Text);
+                Debug.WriteLine($"1-{number1} + {CurrentState}") ;
+            }
+            else
+            {
+                number2 = Convert.ToDouble(result.Text);
+                Debug.WriteLine($"2-{number2} + {CurrentState}");
+            }
+        }
+
+        private void OnMathOperSelect(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            CurrentState = -2;
+            
+            result.Text += button.Text;
+            string btnpress = button.Text;
+            MathOperator = btnpress;
+        }
+
+        private void OnCalc(object sender, EventArgs e)
+        {
+            if (number1 == -1 || number2 == -1)
+            {
+                result.Text = string.Empty;
+                result.FontSize = 24;
+                result.Text += "Не всі числа введенні";
+            }
+            else
+            {
+                double _result = Calculate.Calc(number1, number2, MathOperator);
+                result.Text = string.Empty;
+                result.Text += _result.ToString();
+                number1 = _result;
+            }
+        }
+       
+        private void OnRootElem(object sender, EventArgs e)
+        {
+            if (number1 == 0)
+            {
+                return;
+            }
+            result.Text = (number1 * number1).ToString();
+        }
+    }
+
+}
